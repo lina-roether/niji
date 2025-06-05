35f16969d55ce125de96b84c6c749e40fbe3e393
lina-roether <lina.roether@proton.me>
2025-06-05 21:17:02 +0200

    feat(modules/hyprland): Add warning if not sourced

--- assets/modules/hyprland/module.lua
+++ assets/modules/hyprland/module.lua
@@ -2,6 +2,25 @@ local M = {}
 
 local template = niji.Template:load("theme.conf.mustache")
 
+local function warn_if_not_sourced(config)
+	if config.suppress_not_sourced_warning then
+		return
+	end
+
+	local hyprland_config = niji.fs.read_config("hypr/hyprland.conf");
+
+	if not string.match(hyprland_config, "[\\^\n]%s*source%s+=.*niji/hyprland/theme%.conf") then
+		niji.console.warn(
+			"You don't seem to have sourced niji's generated config for hyprland!\n" ..
+			"\n" ..
+			"To do this, add the following line to your hyprland.conf:\n" ..
+			"source = " .. niji.fs.get_output_dir() .. "/theme.conf\n" ..
+			"\n" ..
+			"To suppress this warning instead, set suppress_not_sourced_warning in the module options."
+		)
+	end
+end
+
 function M.apply(config, theme)
 	local configure_cursor = config.cursor_theme ~= nil and config.cursor_size ~= nil
 	if configure_cursor then
@@ -18,6 +37,8 @@ function M.apply(config, theme)
 	}
 
 	niji.fs.output("theme.conf", theme_conf)
+
+	warn_if_not_sourced(config)
 end
 
 function M.reload()
