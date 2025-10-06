b416f90de5ee92c5ddd72a0d944011d0b00e6b4f
lina-roether <lina.roether@proton.me>
2025-10-06 20:39:18 +0200

    test: add unit tests for niji::files

--- crates/niji/src/files.rs
+++ crates/niji/src/files.rs
@@ -1,13 +1,13 @@
 use std::{
 	fs,
-	path::{Path, PathBuf},
+	path::{Path, PathBuf}
 };
 
 use anyhow::Context;
 
 use crate::utils::{
 	fs::{find_dirs, find_files},
-	xdg::XdgDirs,
+	xdg::XdgDirs
 };
 
 #[derive(Debug)]
@@ -17,13 +17,13 @@ pub struct Files {
 	managed_files_file: PathBuf,
 	output_dir: PathBuf,
 	themes_dirs: Vec<PathBuf>,
-	modules_dirs: Vec<PathBuf>,
+	modules_dirs: Vec<PathBuf>
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Location {
 	pub name: String,
-	pub path: PathBuf,
+	pub path: PathBuf
 }
 
 impl Files {
@@ -74,7 +74,7 @@ impl Files {
 			current_theme_file,
 			managed_files_file,
 			themes_dirs,
-			modules_dirs,
+			modules_dirs
 		})
 	}
 
@@ -105,7 +105,7 @@ impl Files {
 				.unwrap_or_default()
 				.to_string_lossy()
 				.to_string(),
-			path: f,
+			path: f
 		});
 		let nested_themes = find_dirs(&self.themes_dirs)
 			.map(|d| Location {
@@ -114,7 +114,7 @@ impl Files {
 					.unwrap_or_default()
 					.to_string_lossy()
 					.to_string(),
-				path: d.join(Self::THEME_MAIN_FILE_NAME),
+				path: d.join(Self::THEME_MAIN_FILE_NAME)
 			})
 			.filter(|l| l.path.exists());
 		toplevel_themes.chain(nested_themes)
@@ -127,11 +127,145 @@ impl Files {
 				.unwrap_or_default()
 				.to_string_lossy()
 				.to_string(),
-			path: d,
+			path: d
 		})
 	}
 }
 
 fn init_dir(dir: &Path) -> anyhow::Result<()> {
 	fs::create_dir_all(dir).context(format!("Failed to create {}", dir.display()))
+}
+
+#[cfg(test)]
+mod tests {
+	use tempfile::tempdir;
+
+	use super::*;
+
+	#[test]
+	fn init() {
+		let tempdir = tempdir().unwrap();
+		let xdg_dirs = XdgDirs::in_tempdir(&tempdir);
+		let files = Files::new(&xdg_dirs).unwrap();
+
+		assert!(xdg_dirs.config_home.join("niji").exists());
+		assert!(xdg_dirs.config_home.join("niji/themes").exists());
+		assert!(xdg_dirs.config_home.join("niji/modules").exists());
+		assert!(xdg_dirs.data_home.join("niji").exists());
+		assert!(xdg_dirs.state_home.join("niji").exists());
+
+		assert_eq!(
+			files.config_file(),
+			xdg_dirs.config_home.join("niji/config.toml")
+		);
+		assert_eq!(
+			files.current_theme_file(),
+			xdg_dirs.state_home.join("niji/current_theme.txt")
+		);
+		assert_eq!(
+			files.managed_files_file(),
+			xdg_dirs.state_home.join("niji/managed_files.csv")
+		);
+		assert_eq!(files.output_dir(), xdg_dirs.data_home.join("niji"));
+	}
+
+	#[test]
+	fn iter_theme_files() {
+		let tempdir = tempdir().unwrap();
+		let xdg_dirs = XdgDirs::in_tempdir(&tempdir);
+		let files = Files::new(&xdg_dirs).unwrap();
+
+		fs::create_dir_all(xdg_dirs.data_dirs[0].join("niji/themes")).unwrap();
+		fs::write(xdg_dirs.data_dirs[0].join("niji/themes/test.toml"), "").unwrap();
+
+		fs::write(xdg_dirs.config_home.join("niji/themes/test2.toml"), "").unwrap();
+
+		let themes: Vec<Location> = files.iter_themes().collect();
+		assert_eq!(
+			themes,
+			vec![
+				Location {
+					name: "test2".to_string(),
+					path: xdg_dirs.config_home.join("niji/themes/test2.toml")
+				},
+				Location {
+					name: "test".to_string(),
+					path: xdg_dirs.data_dirs[0].join("niji/themes/test.toml")
+				}
+			]
+		);
+	}
+
+	#[test]
+	fn iter_theme_folders() {
+		let tempdir = tempdir().unwrap();
+		let xdg_dirs = XdgDirs::in_tempdir(&tempdir);
+		let files = Files::new(&xdg_dirs).unwrap();
+
+		fs::create_dir_all(xdg_dirs.data_dirs[0].join("niji/themes/test")).unwrap();
+		fs::write(
+			xdg_dirs.data_dirs[0].join("niji/themes/test/theme.toml"),
+			""
+		)
+		.unwrap();
+		fs::create_dir(xdg_dirs.config_home.join("niji/themes/test2")).unwrap();
+		fs::write(
+			xdg_dirs.config_home.join("niji/themes/test2/theme.toml"),
+			""
+		)
+		.unwrap();
+
+		let themes: Vec<Location> = files.iter_themes().collect();
+		assert_eq!(
+			themes,
+			vec![
+				Location {
+					name: "test2".to_string(),
+					path: xdg_dirs.config_home.join("niji/themes/test2/theme.toml")
+				},
+				Location {
+					name: "test".to_string(),
+					path: xdg_dirs.data_dirs[0].join("niji/themes/test/theme.toml")
+				}
+			]
+		);
+	}
+
+	#[test]
+	fn iter_empty_theme_folders() {
+		let tempdir = tempdir().unwrap();
+		let xdg_dirs = XdgDirs::in_tempdir(&tempdir);
+		let files = Files::new(&xdg_dirs).unwrap();
+
+		fs::create_dir_all(xdg_dirs.data_dirs[0].join("niji/themes/test")).unwrap();
+		fs::create_dir(xdg_dirs.config_home.join("niji/themes/test2")).unwrap();
+
+		let themes: Vec<Location> = files.iter_themes().collect();
+		assert_eq!(themes, vec![]);
+	}
+
+	#[test]
+	fn iter_module_folders() {
+		let tempdir = tempdir().unwrap();
+		let xdg_dirs = XdgDirs::in_tempdir(&tempdir);
+		let files = Files::new(&xdg_dirs).unwrap();
+
+		fs::create_dir_all(xdg_dirs.data_dirs[0].join("niji/modules/test")).unwrap();
+		fs::create_dir(xdg_dirs.config_home.join("niji/modules/test2")).unwrap();
+
+		let themes: Vec<Location> = files.iter_modules().collect();
+		assert_eq!(
+			themes,
+			vec![
+				Location {
+					name: "test2".to_string(),
+					path: xdg_dirs.config_home.join("niji/modules/test2")
+				},
+				Location {
+					name: "test".to_string(),
+					path: xdg_dirs.data_dirs[0].join("niji/modules/test")
+				}
+			]
+		);
+	}
 }
