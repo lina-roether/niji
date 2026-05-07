fa5702148481c569983bc6c845931ade34dd1e8d
lina-roether <lina.roether@proton.me>
2026-05-07 14:52:13 +0200

    test: remove version test
    it's dumb and annoying

--- crates/niji/tests/general.rs
+++ crates/niji/tests/general.rs
@@ -1,12 +1,6 @@
 use assert_cmd::cargo::cargo_bin_cmd;
 
 #[test]
-fn version() {
-	let cmd = cargo_bin_cmd!("niji").arg("--version").assert();
-	cmd.success().stdout("niji 0.5.1\n");
-}
-
-#[test]
 fn help_option() {
 	let cmd = cargo_bin_cmd!("niji").arg("--help").assert();
 	cmd.success();
